cdfdfaaf6c4f87cd51afdd8ccae227e81331aed4
Diana Zinevych <dianazinevych02@gmail.com>
2025-04-13 21:04:59 +0200

    correct error

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { PersistGate } from 'redux-persist/integration/react';
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from "./redux/store.js";
-import { persistor } from "./redux/store.js";
+
 
 //<StrictMode> — інструмент React для перевірки потенційних проблем.
 //<Provider> — робить Redux store доступним для всіх компонентів.
-//для збереження даних  в локальному сховищі браузера використ PersistGate persistor={persistor}
-//блокує рендеринг <App />, поки не буде завантажено стан з локального сховища.
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      
-      <PersistGate persistor={persistor}>
         <App />
-        </PersistGate>
     </Provider>
   </StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,42 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import contactsReducer from "./contactsSlice";
 import filtersReducer from "./filtersSlice";
-import {
-  persistStore,
-    persistReducer,
-   FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
 
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-//зберігаємо обґєкт балансу (при оновлені редакс стан)
-const contactPersistConfig = {
-  key: "contact",
-  storage,
-};
-//робимо копію нашого редюсера із конфігурацією роботи з локал стор
-// якщо змінюється щось у редюсері контактів зберігається в локал стор
-const persistedContactReducer = persistReducer(
-  contactPersistConfig,
-  contactsReducer
-); 
 export const store = configureStore({
     reducer: {
-        contacts: persistedContactReducer,
+        contacts: contactsReducer,
         filters: filtersReducer,
 
     },
-    // ігнорування функцій в екшенах
-    middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-// за допомогою persistStore створюємо копію всього нашого стора, коли першй раз завантажується додаток прочитало шо в локал стор
-export const persistor = persistStore(store);
